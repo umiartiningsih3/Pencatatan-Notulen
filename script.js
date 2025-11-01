8f083fc033ba1caf0a5419dd405c12442ce35010
Umiarti Ningsih <umiarti.ningsih3@gmail.com>
2025-11-01 14:04:03 +0700

    delete css main content dan js

--- script.js
+++
@@ -1,8 +0,0 @@
-function tambahAgenda() {
-  const ol = document.getElementById('noteList');
-  const li = document.createElement('li');
-  li.contentEditable = "true";
-  li.textContent = "Tulis catatan baru di sini...";
-  ol.appendChild(li);
-  li.focus();
-}
