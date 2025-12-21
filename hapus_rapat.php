4b5fe12a3e141acf1daef20fdbe7ee07ad4a726e
Umiarti Ningsih <umiarti.ningsih3@gmail.com>
2025-12-21 20:44:23 +0700

    revisi hapus rapat

--- hapus_rapat.php
+++ hapus_rapat.php
@@ -6,13 +6,10 @@ if (!$conn) {
 
 $id = $_GET['id'];
 
-// Hapus detail rapat berdasarkan id_rapat
 mysqli_query($conn, "DELETE FROM rapat_detail WHERE id_rapat = '$id'");
 
-// Hapus data utama rapat
 mysqli_query($conn, "DELETE FROM rapat WHERE id = '$id'");
 
-// Kembali ke halaman daftar
 header("Location: daftar_notulen.php");
 exit();
 ?>
