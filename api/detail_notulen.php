a6228b8889e90aa634da3a6a923edec704edb9e0
Umiarti Ningsih <umiarti.ningsih3@gmail.com>
2025-12-26 21:18:52 +0700

    revisi detail notulen

--- api/detail_notulen.php
+++ api/detail_notulen.php
@@ -9,6 +9,7 @@ $h = mysqli_query($conn,"SELECT * FROM hasil_rapat WHERE rapat_id='$id'");
 <head>
 <title>Detail Notulen</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
 
